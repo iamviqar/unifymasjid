3097ee4c28e09ce798cdef1bdd6bd272075c0e17
Mohammed Viqar <viqar.04@gmail.com>
2022-09-14 10:00:24 +0530

    first commit

---
+++ src/main/java/com/hqv/unifymasjid/UnifymasjidApplication.java
@@ -0,0 +1,13 @@
+package com.hqv.unifymasjid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifymasjidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifymasjidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hqv/unifymasjid/UnifymasjidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hqv.unifymasjid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifymasjidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
